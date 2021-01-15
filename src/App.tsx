70823b56d0c4601f1085fc7aab71d2bfc48ad859
Martin Mohn <mohn@hiscout.com>
2021-01-15 14:38:49 +0100

    Add agenda

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Part1 from "./parts/part1";
 
 const App: React.FC = () => {
-  return <div>Hello </div>;
+  return (
+    <>
+      <h1>Workshop</h1>
+      <Part1 />
+      {/* <Part2 />
+      <Part3 />
+      <Part4 />
+      <Part5 />
+      <Part6 /> */}
+    </>
+  );
 };
 
 export default App;
---
+++ src/parts/part1.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Part1: React.FC = () => {
+  return (
+    <>
+      <h2>Part 1</h2>
+      <div>Inhalt</div>
+    </>
+  );
+};
+
+export default Part1;
